2d6f80085af3e1403e6930f553fd22fa0543d11d
SAIKUMARAR7 <42142731+SAIKUMARAR7@users.noreply.github.com>
2020-09-17 17:15:09 +0530

    JoinRequest Authentication Done

--- Signup_Module/MDA/socius/models.py
+++ Signup_Module/MDA/socius/models.py
@@ -40,6 +40,7 @@ class DirectoryMembers(models.Model):
     DirectoryId=models.CharField(max_length=10,default='')
     Email=models.EmailField(max_length=250,blank=True)
     Bio=models.TextField(max_length=250,blank=True,null=True)
+    active=models.BooleanField(blank=False,default=False,null=False)
     user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='user_id',default='',null=True)
     #def __str__(self):
      #  return self.Directory.DirectoryName
